d0b63ac31beb6ecd353749594b61dd9e28e53204
simplyjuanc <juancamilovasquezj@gmail.com>
2022-06-07 06:07:20 +0100

    Initialise index.html and script.js. Add computerPlay function

---
+++ script.js
@@ -0,0 +1,11 @@
+function computerPlay() {
+    options = ['Rock', 'Paper', 'Scissors'];
+    rand_choice = Math.floor(Math.random() * 3);
+    return options[rand_choice];
+};
+
+
+
+function playRound(playeSelection, computerSelection) {
+
+}
