d58df251dd8d7eaf540a67a1588d9655d735f4c8
simplyjuanc <juancamilovasquezj@gmail.com>
2022-06-08 17:34:13 +0100

    Add playRound function to determine winning pair

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+// computerPlay function returns one of the three Rock, Paper, Scissor options
 function computerPlay() {
-    options = ['Rock', 'Paper', 'Scissors'];
-    rand_choice = Math.floor(Math.random() * 3);
+    let options = ['Rock', 'Paper', 'Scissors'];
+    const rand_choice = Math.floor(Math.random() * 3);
     return options[rand_choice];
 };
 
 
 
-function playRound(playeSelection, computerSelection) {
+function playRound(computerSelection) {
+    // Defines how each of the pairs in the game beats the other
+    let pairs = {
+        'rock': ['Paper', 'Scissors'],
+        'paper': ['Scissors', 'Rock'],
+        'scissors': ['Rock', 'Paper']
+    };
 
+
+    let playerSelection = prompt('Please enter your choice: Rock, Paper, Scissors').toLowerCase()
+    console.log(playerSelection)
+
+
+    // Compares the hand of the first player to that of the second determines which hand is superior
+    if (computerSelection == pairs[playerSelection][0]) {
+        const winner = 'Player';
+    } else if (computerSelection == pairs[playerSelection][1]) {
+        const winner = 'Computer'
+    } else {
+        const winner = 'Tie'
+    }
+
+
+
+    // console.log(winner)
+    // return winner
 }
