16a5df5d894630808c733770515adeb40206b98a
simplyjuanc_lr <juan.vasquez@liveramp.com>
2022-06-15 21:08:53 +0100

    Add playerPlay func, simplify return of playRound and correct matching pairs

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function playerPlay() {
+    return prompt('Please enter your choice: Rock, Paper, Scissors').toLowerCase()
+}
+
+
 // computerPlay function returns one of the three Rock, Paper, Scissor options
 function computerPlay() {
     let options = ['Rock', 'Paper', 'Scissors'];
@@ -7,30 +12,30 @@ function computerPlay() {
 
 
 
-function playRound(computerSelection) {
-    // Defines how each of the pairs in the game beats the other
+function playRound(computerSelection, playerSelection) {
+    // Defines how each of the pairs in the game beats the other (is beaten by 0 and beats 1)
     let pairs = {
         'rock': ['Paper', 'Scissors'],
         'paper': ['Scissors', 'Rock'],
         'scissors': ['Rock', 'Paper']
     };
 
-
-    let playerSelection = prompt('Please enter your choice: Rock, Paper, Scissors').toLowerCase()
-    console.log(playerSelection)
-
-
     // Compares the hand of the first player to that of the second determines which hand is superior
-    if (computerSelection == pairs[playerSelection][0]) {
-        const winner = 'Player';
-    } else if (computerSelection == pairs[playerSelection][1]) {
-        const winner = 'Computer'
+    if (computerSelection == pairs[playerSelection][1]) {
+        return 'Player';
+    } else if (computerSelection == pairs[playerSelection][0]) {
+        return 'Computer';
     } else {
-        const winner = 'Tie'
+        return 'Tie';
     }
 
-
-
-    // console.log(winner)
-    // return winner
+    // You can make it so that if player wins =+ 1, if computer wins -=1 and if tie then + 0
 }
+
+
+
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+
+
+console.log(playRound(computerSelection, playerSelection));
