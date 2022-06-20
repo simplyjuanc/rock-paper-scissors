f140ebb0197e0a9d945a08f09bf882c6ba664cf9
simplyjuanc_lr <juan.vasquez@liveramp.com>
2022-06-20 18:32:59 +0100

    Add basic buttons/UI to game

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function playerPlay() {
-    return prompt('Please enter your choice: Rock, Paper, Scissors').toLowerCase()
-};
-
-
 // computerPlay function returns one of the three Rock, Paper, Scissor options
 function computerPlay() {
     let options = ['Rock', 'Paper', 'Scissors'];
     const rand_choice = Math.floor(Math.random() * 3);
     return options[rand_choice];
-};
-
+}
 
 
 function playRound(computerSelection, playerSelection) {
@@ -20,47 +14,64 @@ function playRound(computerSelection, playerSelection) {
         'scissors': ['Rock', 'Paper']
     };
 
+
     // Compares the hand of the first player to that of the second determines which hand is superior
     // YIf player wins =+ 1, if computer wins -=1 and if tie then + 0
+    console.log(`Computer: ${computerSelection}`);
+    console.log(`Player: ${playerSelection}`);
     if (computerSelection == pairs[playerSelection][1]) {
-        return 1;
+        ++playerTally;
     } else if (computerSelection == pairs[playerSelection][0]) {
-        return -1;
-    } else {
-        return 0;
+        ++computerTally;
     }
+    console.log(`PC tally: ${computerTally}`);
+    console.log(`Player tally: ${playerTally}`);
 
+    const p = document.createElement('p');
+    p.textContent = `Current score: ${computerTally} (Computer) vs ${playerTally} (Player)`;
+    results.appendChild(p);
 };
 
 
+function playerPlay(e) {
+    let playerSelection = e.target.id;
+    let computerSelection = computerPlay();
+    // console.log(`Player Selection: ${e.target.id}`);
+    // console.log(`Computer Selection: ${computerSelection}`);
+    playRound(computerSelection, playerSelection);
+}
 
 
-
-function game() {
-    // The function return the winner of a 5-round game of RockPaper-Scissors
-    // It tallies the score across rounds and compares after it has looped through all of them.
-    let tally = 0;
-    let count_game = 0;
-    while (count_game < 5) {
-        const computerSelection = computerPlay();
-        const playerSelection = playerPlay();
-        console.log(`Player choice: ${playerSelection}`);
-        console.log(`AI choice: ${computerSelection}`);
-        tally += playRound(computerSelection, playerSelection);
-        count_game++;
-    }
-
-    if (tally > 0) {
-        return 'Player wins';
-    } else if (tally < 0) {
-        return 'Computer wins';
+function declareWinner() {
+    const finalOutcome = document.createElement('h2');
+    if (playerTally > computerTally) {
+        finalOutcome.textContent = 'Player won!';
     } else {
-        return 'It\'s a tie';
+        finalOutcome.textContent = 'Computer won!';
     }
-};
+    results.appendChild(finalOutcome);
+}
+
+
+let computerTally = 0;
+let playerTally = 0;
+let playOptions = document.querySelectorAll('button');
+let results = document.querySelector('.result');
+const p = document.createElement('p')
+
+const finalOutcome = document.createElement('h2');
+playOptions.forEach(button => button.addEventListener('click', playerPlay));
+playOptions.forEach(button => button.addEventListener('click', function() {
+    if (playerTally == 5 || computerTally == 5) declareWinner();
+}));
 
 
 
+// if () {
+
+// }
 
 
-console.log(game());
+// Initilaise tallies for both players  
+// Add the reult of the round to the div in the HTML as text
+// Add to the tally of either player based on round results
